44cc8c1e3307a5d9f7b991a054e087e5f9fbfe8f
khandgonda <akshaykhandgonda@gmail.com>
2019-04-19 22:10:12 -0400

    Able to get dates and navigate between months

--- calendar.js
+++ calendar.js
@@ -5,6 +5,7 @@
  *
  * @param date a JavaScript Date object set to a day in the month to be displayed.
  */
+var showDate;
 var update = function(date) {
   var months = [
     "January",
@@ -20,11 +21,32 @@ var update = function(date) {
     "November",
     "December"
   ];
+
   var daysInCurrentMonth = new Date(
     date.getFullYear(),
-    date.getMonth() + 1,
-    0
+    date.getMonth() + 1
   ).getDate();
+
+  document.getElementById("monthHeader").innerHTML = months[date.getMonth()];
+  document.getElementById("yearHeader").innerHTML = date.getFullYear();
+
+  showDate = date;
+  var d = date;
+  d.setDate(1);
+  var day = d.getDay();
+  var dayOne = day;
+  var b = 1;
+  var doc = document.getElementById("calendarTable");
+  for (var x = dayOne; x <= daysInCurrentMonth + (dayOne - 1); x++) {
+    doc.getElementsByTagName("td")[x].innerHTML = b;
+    b++;
+  }
+  for (var x = dayOne - 1; x >= 0; x--) {
+    doc.getElementsByTagName("td")[x].innerHTML = "&nbsp;";
+  }
+  for (var x = daysInCurrentMonth + dayOne; x < 42; x++) {
+    doc.getElementsByTagName("td")[x].innerHTML = "&nbsp;";
+  }
 
   //   var year = getElementsById("form_year");
 
@@ -33,50 +55,24 @@ var update = function(date) {
   // Calculate the previous and next month
   // (You may use this for adding links to the left arrow)
 };
-// function isFourDigitYear(year) {
-//   if (year.length != 4) {
-//     alert("The year must be four-digits");
-//   } else {
-//     return true;
-//   }
-// }
 
 function next() {
-  //   var nextMonth = getElementsByTagName("form_month");
-  var nextMonth = date.getMonth() + 1;
-  var nextYear = date.getFullYear();
+  var nextMonth = showDate.getMonth() + 1;
+  var nextYear = showDate.getFullYear();
   if (nextMonth >= 12) {
     // Remember:  Months are numbered beginning with 0.
     nextMonth = 0;
     nextYear++;
   }
+  update(new Date(nextYear, nextMonth, 1));
 }
 
 function previous() {
-  //   var previousMonth = getElementsByTagName("form_year");
-  var previousMonth = date.getMonth() - 1;
-  var previousYear = date.getFullYear();
-  if (previousMonth < 0) {
-    // Remember:  Months are numbered beginning with 0.
-    previousMonth = 11;
-    previousYear--;
+  var preMonth = showDate.getMonth() - 1;
+  var preYear = showDate.getFullYear();
+  if (preMonth < 0) {
+    preMonth = 11;
+    preYear--;
   }
-}
-
-function calendar() {
-  var febDays = "";
-
-  if (month == 1) {
-    if ((year % 100 != 0 && year % 4 == 0) || year % 400 == 0) {
-      febDays = 29;
-    } else {
-      febDays = 28;
-    }
-  }
-}
-
-function showDates() {
-  var d = new Date();
-  var n = d.getDate();
-  document.getElementsByTagName("tr").innerHTML = n;
+  update(new Date(preYear, preMonth, 1));
 }
